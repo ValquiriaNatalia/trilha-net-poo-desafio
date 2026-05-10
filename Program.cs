38a3340e13c7c64cad7c3e259c750d6d08f2b1c8
valquiria <valquirianatalia0@gmail.com>
2026-05-10 20:28:16 -0300

    Implementando Iphone e Nokia

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("4444","Nokia","11234679",58);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhastApp");
+
+Console.WriteLine("==================================================");
+
+Iphone iphone = new Iphone("55555","Iphone 11","12345678",103);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
